2448d1e5ae3be7121ffd19c5b22f9d472a26b6c9
leenabhari <144324130+leenabhari@users.noreply.github.com>
2024-11-18 22:19:17 +0400

    FIXED BUTTON CLICKING WOO

--- scrapper.py
+++ scrapper.py
@@ -9,6 +9,7 @@ import requests as rq
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver as se
 from selenium.webdriver.common.by import By
+import time
 
 #URL = "https://www.rit.edu/dubai/directory"
 
@@ -16,11 +17,16 @@ from selenium.webdriver.common.by import By
 
 #soup = bs(page.text, "html.parser")
 
-
 driver = se.Chrome()
 
 driver.get("https://www.rit.edu/dubai/directory")
 
-button = driver.find_element(By.CSS_SELECTOR,"see-more")
+#for loop to click 'load more' 5 times
+for i in range(5):
+        button = driver.find_element(By.CLASS_NAME,"see-more") #selecting the element from its class
+        driver.execute_script("arguments[0].scrollIntoView(true);", button) #scrolls to element so that it can be clickable (doesnt work without this for sum reason)
+        button.click()
+        time.sleep(5) #waits for 5 seconds to give the page time to load
 
-button.click()
+input("enter to exit") #for now.. testing purposes only :3
+driver.quit()
