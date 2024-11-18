712c31f5f82aa90a9fcc9b5b7cd8eb3131019de1
leenabhari <144324130+leenabhari@users.noreply.github.com>
2024-11-18 11:54:10 +0400

    started scrapper page

--- scrapper.py
+++ scrapper.py
@@ -5,3 +5,22 @@
 #############################################################
 
 
+import requests as rq
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver as se
+from selenium.webdriver.common.by import By
+
+#URL = "https://www.rit.edu/dubai/directory"
+
+#page = rq.get(URL)
+
+#soup = bs(page.text, "html.parser")
+
+
+driver = se.Chrome()
+
+driver.get("https://www.rit.edu/dubai/directory")
+
+click = driver.find_element(By.CLASS_NAME,"see-more")
+print(click)
+
