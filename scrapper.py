a75302b31b6c93aa10a6b0ae7649e08ba1be9daf
leenabhari <144324130+leenabhari@users.noreply.github.com>
2024-11-19 14:54:31 +0400

    attempted

--- scrapper.py
+++ scrapper.py
@@ -10,6 +10,7 @@ from bs4 import BeautifulSoup as bs
 from selenium import webdriver as se
 from selenium.webdriver.common.by import By
 import time
+import pandas as pd
 
 driver = se.Chrome()
 
@@ -20,17 +21,31 @@ for i in range(5):
         button = driver.find_element(By.CLASS_NAME,"see-more") #selecting the element from its class
         driver.execute_script("arguments[0].scrollIntoView(true);", button) #scrolls to element so that it can be clickable (doesnt work without this for sum reason)
         button.click()
-        time.sleep(2) #waits for 5 seconds to give the page time to load
+        time.sleep(3) #waits for 5 seconds to give the page time to load
 
 
 soup = bs(driver.page_source, "html.parser") #to extract html code of the webpage
-
-employee=soup.find_all(class_="views-row")
-count=0
-for e in employee:
-        print(e.getText())
-        count=+1
-
-input("enter to exit") #for now.. testing purposes only :3
-print(count)
 driver.quit()
+#problem is that we need to use requests module 
+
+
+#seconmd problem find way to parse
+employee = soup.select(".pb-2")  # Extracting all lines
+data = [e.get_text(strip=True) for e in employee]  # Clean text
+
+# Group the lines into sets of four (Name, Title, Workplace, Email)
+structured_data = []
+for i in range(0, len(data)):  # Process every 4 lines
+    if i + 3 < len(data):  # Ensure there are enough lines for a complete record
+        name = data[i]
+        title = data[i + 1]
+        # Skipping workplace line (data[i + 2]) as it's not needed
+        email = data[i + 3]
+        structured_data.append([name, title, email])
+
+# Create a DataFrame
+df = pd.DataFrame(structured_data, columns=["Name", "Title", "Email"])
+
+# Save to CSV
+df.to_csv("directory.csv", index=False)
+
