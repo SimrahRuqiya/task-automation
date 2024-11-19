964afb8d670b30b4312b162fda4509beea0ae5ab
SimrahRuqiya <123015484+SimrahRuqiya@users.noreply.github.com>
2024-11-19 10:47:15 +0400

    scrapper loading 5 times done with print

--- scrapper.py
+++ scrapper.py
@@ -11,12 +11,6 @@ from selenium import webdriver as se
 from selenium.webdriver.common.by import By
 import time
 
-#URL = "https://www.rit.edu/dubai/directory"
-
-#page = rq.get(URL)
-
-#soup = bs(page.text, "html.parser")
-
 driver = se.Chrome()
 
 driver.get("https://www.rit.edu/dubai/directory")
@@ -26,7 +20,17 @@ for i in range(5):
         button = driver.find_element(By.CLASS_NAME,"see-more") #selecting the element from its class
         driver.execute_script("arguments[0].scrollIntoView(true);", button) #scrolls to element so that it can be clickable (doesnt work without this for sum reason)
         button.click()
-        time.sleep(5) #waits for 5 seconds to give the page time to load
+        time.sleep(2) #waits for 5 seconds to give the page time to load
+
+
+soup = bs(driver.page_source, "html.parser") #to extract html code of the webpage
+
+employee=soup.find_all(class_="views-row")
+count=0
+for e in employee:
+        print(e.getText())
+        count=+1
 
 input("enter to exit") #for now.. testing purposes only :3
+print(count)
 driver.quit()
