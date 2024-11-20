d3e35ac28e1fdc1982d17cd72f37d49429ca93b8
leenabhari <144324130+leenabhari@users.noreply.github.com>
2024-11-20 11:22:29 +0400

    FIXED SCRAPPERRR

--- scrapper.py
+++ scrapper.py
@@ -1,9 +1,6 @@
-
-
 #############################################################
 ### use this file to implement the web scrapper in part 1 ###
 #############################################################
-
 
 import requests as rq
 from bs4 import BeautifulSoup as bs
@@ -11,6 +8,7 @@ from selenium import webdriver as se
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
+import re
 
 driver = se.Chrome()
 
@@ -21,30 +19,33 @@ for i in range(5):
         button = driver.find_element(By.CLASS_NAME,"see-more") #selecting the element from its class
         driver.execute_script("arguments[0].scrollIntoView(true);", button) #scrolls to element so that it can be clickable (doesnt work without this for sum reason)
         button.click()
-        time.sleep(3) #waits for 5 seconds to give the page time to load
+        time.sleep(3) #waits for 3 seconds to give the page time to load
 
-
+#using soup from the loaded page
 soup = bs(driver.page_source, "html.parser") #to extract html code of the webpage
 driver.quit()
-#problem is that we need to use requests module 
 
 
-#seconmd problem find way to parse
-employee = soup.select(".pb-2")  # Extracting all lines
-data = [e.get_text(strip=True) for e in employee]  # Clean text
 
-# Group the lines into sets of four (Name, Title, Workplace, Email)
-structured_data = []
-for i in range(0, len(data), 4):  # Process every 4 lines
-    if i + 3 < len(data):  # Ensure there are enough lines for a complete record
-        name = data[i]
-        title = data[i + 1]
-        # Skipping workplace line (data[i + 2]) as it's not needed
-        email = data[i + 3]
-        structured_data.append([name, title, email])
+employee_blocks = soup.select('div.col-xs-12.col-sm-5.person--info')  # selecting the div that includes the information of employees
+employees = [] #empty list for employees
 
-# Create a DataFrame
-df = pd.DataFrame(structured_data, columns=["Name", "Title", "Email"])
+    
+for block in employee_blocks:
+        # extract name
+        name = block.select_one('div.pb-2 a').get_text(strip=True) if block.select_one('div.pb-2 a') else "N/A"
+        
+        # extract title
+        title = block.select_one('div.pb-2.directory-text-small').get_text(strip=True) if block.select_one('div.pb-2.directory-text-small') else "N/A"
+        
+        # extract email
+        email_block = block.find_next_sibling('div', class_='col-xs-12 col-sm-4 person--extra-text')
+        email = email_block.select_one('a[href^="mailto:"]').get_text(strip=True) if email_block and email_block.select_one('a[href^="mailto:"]') else "N/A"
+        
+        # append to the list
+        employees.append({'Name': name, 'Title': title, 'Email': email})
+    
+df = pd.DataFrame(employees, columns=["Name", "Title", "Email"])
 
 # Save to CSV
 df.to_csv("directory.csv", index=False)
