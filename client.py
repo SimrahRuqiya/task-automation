1dd9db8d51d509d0032eff6289ea1589bcccec4c
SimrahRuqiya <123015484+SimrahRuqiya@users.noreply.github.com>
2024-11-21 11:25:03 +0400

    adding random commands in server and client

--- client.py
+++ client.py
@@ -2,3 +2,15 @@
 ### use this file to implement the client-side in part 2 ###
 ############################################################
 
+import socket
+import xml.etree.ElementTree as ET
+#client reads xml file
+
+xml=input("Enter XML fil: ")
+
+p = ET.parse(xml)
+root=p.getroot()
+
+host_name=socket.gethostname()
+port=12345
+client_socket=socket.socket()
--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-
-
 ############################################################
 ### use this file to implement the server-side in part 2 ###
 ############################################################
 
+import socket
 
+host_name=socket.gethostname()
+port=12345
+server_socket=socket.socket()
+server_socket.bind(host_name,port)
+server_socket.listen()
